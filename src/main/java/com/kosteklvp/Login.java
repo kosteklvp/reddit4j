c2f69edebc4265e8746b8c89ad511bfa0fa28170
Piotr Kostański <kostek.lvp@gmail.com>
2021-04-19 20:17:50 +0200

    b

--- src/main/java/com/kosteklvp/Login.java
+++
@@ -1,5 +0,0 @@
-package com.kosteklvp;
-
-public class Login {
-
-}
--- src/test/java/com/kosteklvp/table/TableGeneratorTest.java
+++ src/test/java/com/kosteklvp/table/TableGeneratorTest.java
@@ -1,7 +1,7 @@
 package com.kosteklvp.table;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
